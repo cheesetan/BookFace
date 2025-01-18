ff9313ddc4d30004d95c6bbb22745db7f75a85f8
Tristan Chay <tristanchay123@gmail.com>
2025-01-18 22:13:18 +0800

    Initial Commit

---
+++ BookFace/BookFaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookFaceApp.swift
+//  BookFace
+//
+//  Created by Tristan Chay on 18/1/25.
+//
+
+import SwiftUI
+
+@main
+struct BookFaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BookFace/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BookFace
+//
+//  Created by Tristan Chay on 18/1/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
